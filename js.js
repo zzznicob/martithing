d02767fc85a39eb034cf75613a8efdbd52ebda9a
nico <138622764+zzznicob@users.noreply.github.com>
2024-06-07 00:03:47 +0200

    yuh

---
+++ js.js
@@ -0,0 +1,14 @@
+
+$.each('td', function() {
+    var currentTDWidth = $(this).width(); // more code here
+});   
+   
+
+
+
+function equal() {
+var width = $("#pic").width();
+
+$("#pic").height(width);
+
+}
